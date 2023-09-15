f994e3c4584a1296ee96e9bfdf45655375a6b94f
vi-tr <144982881+vi-tr@users.noreply.github.com>
2023-09-15 12:19:15 +0000

    Add appropriate flags for better accuracy
    For some reason the website doesn't specify whether gccgo or gc is
    used, just "Golang 1.21.0". I assume it's GC but added the gcc related
    flags regardless.

--- shell.nix
+++ shell.nix
@@ -4,5 +4,9 @@ with pkgs;
 mkShell {
     nativeBuildInputs = [ gcc7 go ];
 
-    shellHook = "exec dash"; # Feel free to remove
+    shellHook = ''
+        export CFLAGS="-lm -O2 -fno-stack-limit -std=c++1z -x c++"
+        export GOFLAGS="-gccgoflags=-O2 -gccgoflags=-fno-stack-limit'"
+        exec dash # Feel free to remove
+    '';
 }
